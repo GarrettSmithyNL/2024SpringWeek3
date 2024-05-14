6ac164217370e7ec80058493c0c09429e6b780c7
GarrettSmithyNL <garrett_smith_30@hotmail.com>
2024-05-14 14:21:31 -0230

    displayed html

--- index.js
+++ index.js
@@ -1,13 +1,27 @@
 const http = require("http");
 
-serveText("Hello Week 3!");
+// serveText("Hello Week 3!");
 
-function serveText(theText) {
+// function serveText(theText) {
+//   const server = http.createServer(function (req, res) {
+//     console.log("Request received!");
+//     res.writeHead(200, { "Content-Type": "text/plain" });
+//     res.write(theText);
+//     res.write(", eat more lunch!");
+//     res.end();
+//   });
+//   server.listen(3000, () => {
+//     console.log("Server running on port 3000");
+//   });
+// }
+
+serveHTML("<html><body><h1>Hello Week 3!</h1></body></html>");
+
+function serveHTML(theHTML) {
   const server = http.createServer(function (req, res) {
     console.log("Request received!");
-    res.writeHead(200, { "Content-Type": "text/plain" });
-    res.write(theText);
-    res.write(", eat more lunch!");
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.write(theHTML);
     res.end();
   });
   server.listen(3000, () => {
